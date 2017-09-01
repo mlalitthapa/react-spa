c4a1e40981c5f92039de311b810d0fb89b6404f9
Lalit Thapa <mlalitthapa@gmail.com>
2017-09-01 20:45:30 +0545

    add action to fetch posts

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import Posts from './components/posts_index';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={Posts} />
+        </div>
+    </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('#app'));
