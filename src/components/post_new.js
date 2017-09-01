6842596d7a5bcc5eda2aa25820a7be58fb193874
Lalit Thapa <mlalitthapa@gmail.com>
2017-09-01 23:40:59 +0545

    setup routes for new post

---
+++ src/components/post_new.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class PostNew extends Component {
+
+    render() {
+        return (
+            <div>
+                New Post
+            </div>
+        );
+    }
+
+}
+
+export default PostNew;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import reducers from './reducers';
-import Posts from './components/posts_index';
 import promise from 'redux-promise';
+
+import Posts from './components/posts_index';
+import PostNew from './components/post_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -14,7 +16,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
         <div>
-            <Route path="/" component={Posts} />
+            <Switch>
+                <Route path="/posts/new" component={PostNew} />
+                <Route path="/" component={Posts} />
+            </Switch>
         </div>
     </BrowserRouter>
   </Provider>
