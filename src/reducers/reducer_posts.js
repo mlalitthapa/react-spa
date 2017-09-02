fdf1daa1e8c5fdbfa71e9d698e5e2c06989b9180
Lalit Thapa <mlalitthapa@gmail.com>
2017-09-02 16:10:15 +0545

    add action and reducer for show post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import {FETCH_POSTS} from "../actions/index";
+import {FETCH_POSTS, FETCH_POST} from "../actions/index";
 import _ from 'lodash';
 
 export default function (state = {}, action) {
@@ -6,6 +6,9 @@ export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+
+        case FETCH_POST:
+            return {...state, [action.payload.data.id]: action.payload.data};
 
         default:
             return state;
