8a2ade7115e326fa8eea69a7837a7df54f2e2730
Lalit Thapa <mlalitthapa@gmail.com>
2017-09-03 09:09:54 +0545

    show post detail

--- src/components/post_detail.js
+++ src/components/post_detail.js
@@ -1,13 +1,35 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from "../actions/index";
 
 class PostDetail extends Component {
 
+    componentDidMount()
+    {
+        const {id} = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+
     render(){
+
+        const {post} = this.props;
+
+        if(!post)
+            return <div>Loading...</div>;
+
         return (
-            <div>Post Detail</div>
+            <div>
+                <h3>{post.title}</h3>
+                <h5>Categories: {post.categories}</h5>
+                <p>{post.title}</p>
+            </div>
         )
     }
 
 }
 
-export default PostDetail;
+function mapStateToProps({posts}, ownProps) {
+    return {post: posts[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostDetail);
