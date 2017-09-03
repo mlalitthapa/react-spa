3877d8ae28040ffa65db94c6a8b164a278d7e3f9
Lalit Thapa <mlalitthapa@gmail.com>
2017-09-03 09:30:39 +0545

    update local state on deleting post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,12 @@
-import {FETCH_POSTS, FETCH_POST} from "../actions/index";
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from "../actions/index";
 import _ from 'lodash';
 
 export default function (state = {}, action) {
 
     switch (action.type) {
+        case DELETE_POST:
+            return _.omit(state, action.payload);
+
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
 
